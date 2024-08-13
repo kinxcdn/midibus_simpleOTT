bc9048e5068c3431abb0a4d51c04fb4d3b460f13
Ssuhyeong <tngud124@gmail.com>
2024-08-13 15:34:03 +0900

     🔨 refactor : base axios 수정

--- src/apis/instance.js
+++ src/apis/instance.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 import { storage } from "../constants/storage";
 
+const instance = axios.create({
+  baseURL: `https://api-v2.midibus.dev-kinxcdn.com`,
+});
+
 export async function getAuthToken() {
   const value = await storage.getString("authKey");
   return value;
 }
 
-export const authAxios = axios.create({
-  baseURL: `https://api-v2.midibus.dev-kinxcdn.com`,
-});
-
-authAxios.interceptors.request.use(async (config) => {
+instance.interceptors.request.use(async (config) => {
   const _authKey = await getAuthToken();
   if (_authKey) {
     config.headers["X-Mbus-Token"] = _authKey;
@@ -18,3 +18,5 @@ authAxios.interceptors.request.use(async (config) => {
 
   return config;
 });
+
+export { instance };
