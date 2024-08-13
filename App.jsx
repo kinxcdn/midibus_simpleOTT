22d00f1dc2a911d76a44d51b9c73930a3a1622df
Ssuhyeong <tngud124@gmail.com>
2024-08-13 15:32:22 +0900

    ✨ feat : react query 환경 세팅

--- App.jsx
+++ App.jsx
@@ -1,13 +1,20 @@
-import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import StackNavigation from './src/navigations/StackNavigator';
+import StackNavigation from "./src/navigations/StackNavigator";
+
+// QueryClient 인스턴스 생성
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <StackNavigation />
-    </NavigationContainer>
+    // QueryClientProvider로 애플리케이션을 감쌉니다.
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <StackNavigation />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 };
 
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 AppRegistry.registerComponent(appName, () => App);
