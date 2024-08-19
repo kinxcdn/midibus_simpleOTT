690f14e883a67c663054b69b18618b9f56c7b517
Ssuhyeong <tngud124@gmail.com>
2024-08-19 17:08:42 +0900

    🔨 refactor : 파라미터 잘못 기입된 부분 수정

--- src/apis/media/Queries/useGetTagMediaList.js
+++ src/apis/media/Queries/useGetTagMediaList.js
@@ -1,15 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { getTagMediaList } from "../mediaApi";
 
-const useGetTagMediaList = (tagId, categorizedId) => {
+const useGetTagMediaList = (channelId, categorizedId) => {
   const {
     data = [], // 기본값을 빈 배열로 설정하여 undefined 반환을 방지
     isLoading,
     isError,
     error,
   } = useQuery({
-    queryKey: ["mediaListByTag", tagId, categorizedId],
-    queryFn: () => getTagMediaList(tagId, categorizedId),
+    queryKey: ["mediaListByTag", channelId, categorizedId],
+    queryFn: () => getTagMediaList(channelId, categorizedId),
+    enabled: !!categorizedId,
     onError: (err) => {
       console.error("Failed to fetch media list by tag:", err);
     },
